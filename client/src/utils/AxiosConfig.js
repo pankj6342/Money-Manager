7ea1a238ce262d29d8e2df6ec7d4f22927a409ed
pankaj <psbhardwaj.psb@gmail.com>
2024-02-07 00:41:29 +0530

    backend uri change

--- client/src/utils/AxiosConfig.js
+++ client/src/utils/AxiosConfig.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 export const instance = axios.create({
     // baseURL: "https://money-manager-backend-lake.vercel.app/",
     // baseURL:"http://localhost:1234/",
-    baseURL: process.env.NODE_ENV === 'production' ? "https://money-manager-backend-lake.vercel.app/" : "http://localhost:1234/",
+    baseURL: process.env.NODE_ENV === 'production' ? "https://money-manager-backend-49iw.onrender.com/" : "http://localhost:1234/",
     // timeout: 4000,
 //     headers: {'reqcomingfrom': 'web','lang':'en','accept':'json','apikey':'ABCD123'}
 }
